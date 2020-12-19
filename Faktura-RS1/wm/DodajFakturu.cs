f0631f49b3fe763d8dc993bc6e96ef8203c7a795
DemonHuntDW <45897191+DemonHuntDW@users.noreply.github.com>
2020-12-19 19:58:22 +0100

    Add files via upload

---
+++ Faktura-RS1/wm/DodajFakturu.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Faktura.WM
+{
+    public class DodajFakturu
+    {
+        public int klijentID { get; set; }
+
+        public List<SelectListItem> klijentstavke { get; set; }
+
+        public DateTime datum { get; set; }
+
+
+        public int PonudaID { get; set; }
+        public List<SelectListItem> ponudastavke { get; set; }
+
+
+
+
+
+
+
+    }
+}
