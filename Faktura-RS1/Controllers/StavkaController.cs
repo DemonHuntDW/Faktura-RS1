08df740855b7496be0c0b884a908de34a17953cb
DemonHuntDW <45897191+DemonHuntDW@users.noreply.github.com>
2021-01-03 20:27:59 +0100

    Add files via upload

--- Faktura-RS1/Controllers/StavkaController.cs
+++ Faktura-RS1/Controllers/StavkaController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using RS1_Faktura.EF;
 using RS1_Faktura.Models;
 using RS1_Faktura.ViewModels;
@@ -29,5 +30,77 @@ namespace RS1_Faktura.Controllers
             
             return View(m);
         }
+
+        public IActionResult Uredi(int StavkaID)
+        {
+            StavkaUrediVM m = new StavkaUrediVM();
+
+
+            var s = _database.FakturaStavka.Find(StavkaID);
+
+            m.Kolicina = s.Kolicina;
+            m.ProizvodID = s.ProizvodId;
+
+            m.ProizvodStavke = _database
+                .Proizvod
+                .Select(s=>new SelectListItem
+            {
+                Text = s.Naziv + " - " + s.Cijena,
+                Value = s.Id.ToString()
+            }).ToList();
+
+            return View(m);
+        }
+
+        public IActionResult Dodaj(int FakturaID)
+        {
+            var m = new StavkaUrediVM
+            {
+                ProizvodStavke = 
+                    _database
+                    .Proizvod
+                    .Select(s => new SelectListItem
+                    {
+                        Text = s.Naziv + " - " + s.Cijena,
+                        Value = s.Id.ToString()
+                    }).ToList(),
+
+                FakturaID = FakturaID
+            };
+            return View("Uredi", m);
+        }
+
+        public string Obrisi(int StavkaID)
+        {
+            FakturaStavka x = _database.FakturaStavka.Find(StavkaID);
+            _database.Remove(x);
+            _database.SaveChanges();
+
+            return "OK";
+        }
+
+        public string Snimi(StavkaUrediVM x)
+        {
+            FakturaStavka stavka;
+            if (x.StavkaID == 0)
+            {
+                //dodavanje
+                stavka = new FakturaStavka();
+                stavka.FakturaId = x.FakturaID;
+            }
+            else
+            {
+                //edit
+                stavka = _database.FakturaStavka.Find(x.StavkaID);
+            }
+            
+            stavka.Kolicina = x.Kolicina;
+            stavka.ProizvodId = x.ProizvodID;
+            _database.SaveChanges();
+
+            return "OK";
+        }
+
+
     }
 }
