2274cb0d1383657cf87f49a342fa61a3434aaafb
DemonHuntDW <45897191+DemonHuntDW@users.noreply.github.com>
2021-01-03 17:55:08 +0100

    Add files via upload

---
+++ Faktura-RS1/wm/FakturaDetaljiVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RS1_Faktura.Models
+{
+    public class FakturaDetaljiVM
+    {
+        public DateTime Datum { get; set; }
+        public string Klijent { get; set; }
+        public int FakturaID { get; set; }
+    }
+}
