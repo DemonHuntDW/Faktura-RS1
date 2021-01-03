d54c8b078f1aaa27fe34721374ce1e9546033bbd
DemonHuntDW <45897191+DemonHuntDW@users.noreply.github.com>
2021-01-03 20:54:45 +0100

    Add files via upload

---
+++ Faktura-RS1/wm/StavkaUrediVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RS1_Faktura.ViewModels
+{
+    public class StavkaUrediVM
+    {
+        public int ProizvodID { get; set; }
+        public List<SelectListItem> ProizvodStavke { get; set; }
+        public float Kolicina { get; set; }
+        public int StavkaID { get; set; }
+        public int FakturaID { get; set; }
+    }
+}
