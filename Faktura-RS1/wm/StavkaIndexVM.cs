89be3c6184e7f99d5d254073cc5d50ff5765ca43
DemonHuntDW <45897191+DemonHuntDW@users.noreply.github.com>
2021-01-03 18:56:14 +0100

    Add files via upload

---
+++ Faktura-RS1/wm/StavkaIndexVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Faktura.ViewModels
+{
+    public class StavkaIndexVM
+    {
+        public class Row
+        {
+            public int StavkaID { get; set; }
+            public string NazivProizvoda{ get; set; }
+            public float Kolicina{ get; set; }
+            public float Popust{ get; set; }
+            public double Iznos{ get; set; }
+            public float Cijena { get; set; }
+        }
+
+        public List<Row> rows { get; set; }
+        public int FakturaID { get; set; }
+    }
+}
